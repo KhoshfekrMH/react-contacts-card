d2bee91b461924a6f6a217090a0e396b911a9a9d
brainy <khoshfekr2001@protonmail.com>
2022-12-14 02:12:56 +0330

    Apply CSS styles to App.jsx component!

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,15 +5,15 @@ function Card(props) {
         <div>
             <div className="card">
                 <div className="top">
-                    <h2>{props.name}</h2>
-                    <img
+                    <h2 className="name">{props.name}</h2>
+                    <img className="circle-img"
                         src={props.imgURL}
                         alt="avatar-pic"
                     />
                 </div>
                 <div className="bottom">
-                    <p>{props.phone}</p>
-                    <p>{props.email}</p>
+                    <p className="info">{props.phone}</p>
+                    <p className="info">{props.email}</p>
                 </div>
             </div>
         </div>
